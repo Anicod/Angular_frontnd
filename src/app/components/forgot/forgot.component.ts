dc1ff90a499f1dafcd66df97d5847517239fb26d
Anicod <animeshraj735@gmail.com>
2022-07-29 21:27:51 +0530

    added: all components

---
+++ src/app/components/forgot/forgot.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot',
+  templateUrl: './forgot.component.html',
+  styleUrls: ['./forgot.component.scss']
+})
+export class ForgotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  conformPassword(){
+    alert("Your password is set now")
+  }
+
+}
